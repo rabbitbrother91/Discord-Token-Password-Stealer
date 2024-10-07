fcaf360284aa1ddafc1eaf56f908eac7b7172d8f
Everett53443 <hhgsdjagaus@outlook.com>
2024-10-07 15:13:27 +0400

    AssemblyInfo.cs

---
+++ Discord-Stealer/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+
+[assembly: AssemblyVersion("1.2")]
+[assembly: AssemblyTitle("Discord")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Discord")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("8c8325fc-90f7-47e9-b524-374dd6570aed")]
+[assembly: AssemblyFileVersion("1.2")]
